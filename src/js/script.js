52ace0fbc0279f0d7e8d459cb55d051f642e309d
fastmick <michaelhoffmann@gmx.org>
2017-11-03 21:18:13 +0100

    begin js to show full article

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+(function() {
+
+    console.log('viewport: ' + $(".site").outerWidth());
+
+
+    $(".js-full-news-block").click(function() {
+        // alert("Handler for .click() called.");
+        // $(this).parent().addClass("show-full-news-block");
+        $(this).parent().clone().appendTo(".site").addClass("show-full-news-block");
+    });
+
+    // //   alert(navigator.userAgent);
+    // var browser_name = "";
+    // isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    // isEdge = !isIE && !!window.StyleMedia;
+    // if (navigator.userAgent.indexOf("Chrome") != -1 && !isEdge) {
+    //     browser_name = "chrome";
+    // } else if (navigator.userAgent.indexOf("Safari") != -1 && !isEdge) {
+    //     browser_name = "safari";
+    // } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    //     browser_name = "firefox";
+    // } else if (
+    //     navigator.userAgent.indexOf("MSIE") != -1 ||
+    //     !!document.documentMode == true
+    // ) {
+    //     //IF IE > 10
+    //     browser_name = "ie";
+    // } else if (isEdge) {
+    //     browser_name = "edge";
+    // } else {
+    //     browser_name = "other-browser";
+    // }
+    // //     $("html").addClass(browser_name);
+    // $("html").attr("data-browser", browser_name);
+})();
